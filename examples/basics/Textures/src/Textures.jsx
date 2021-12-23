2a326d7ad42e3d65a250c6f4d57b31a06b038362
hazem osama ibrahim <hazem3500@gmail.com>
2021-12-23 09:58:04 +0000

    refactor(examples): Make "Textures" example csb compatible

--- examples/basics/Textures/src/Textures.jsx
+++ examples/basics/Textures/src/Textures.jsx
@@ -4,21 +4,15 @@ import { useControls } from 'leva'
 import React, { Suspense, useEffect } from 'react'
 import * as THREE from 'three'
 
-import map from './assets/gold/color.jpg'
-import displacementMap from './assets/gold/displacement.jpg'
-import metalnessMap from './assets/gold/metalness.jpg'
-import normalMap from './assets/gold/normal.jpg'
-import roughnessMap from './assets/gold/roughness.jpg'
-
 function Box() {
   const { args1 } = useControls({ args1: [1, 32, 32] })
 
   const textureProps = useTexture({
-    map: map.src,
-    displacementMap: displacementMap.src,
-    metalnessMap: metalnessMap.src,
-    normalMap: normalMap.src,
-    roughnessMap: roughnessMap.src,
+    map: 'texture/color.jpg',
+    displacementMap: 'texture/displacement.jpg',
+    metalnessMap: 'texture/metalness.jpg',
+    normalMap: 'texture/normal.jpg',
+    roughnessMap: 'texture/roughness.jpg',
   })
 
   useEffect(() => {
---
+++ examples/basics/Textures/src/index.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import {TexturesExample} from "./Textures";
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(
+  <React.StrictMode>
+      <TexturesExample />
+  </React.StrictMode>,
+  rootElement
+);
