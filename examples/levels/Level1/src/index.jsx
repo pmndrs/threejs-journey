baa6419c3da8e358736245a39d0c433d421accae
— <drcmda@gmail.com>
2021-12-30 18:48:58 +0100

    fix strictmode

--- examples/levels/Level1/src/index.jsx
+++ examples/levels/Level1/src/index.jsx
@@ -1,17 +1,17 @@
 // Original and the models by Bruno Simon: https://threejs-journey.com
 
 import { render } from 'react-dom'
-import { Suspense } from 'react'
+import { StrictMode, Suspense } from 'react'
 import { Loader } from '@react-three/drei'
 import './styles.css'
 import App from './App'
 
 render(
-  <React.StrictMode>
+  <StrictMode>
     <Suspense fallback={null}>
       <App />
     </Suspense>
     <Loader />
-    </React.StrictMode>,
+  </StrictMode>,
   document.getElementById('root'),
 )
