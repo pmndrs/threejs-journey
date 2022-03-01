fc67e092b0817809f92cccbaee868ee51329ec6d
hazem osama ibrahim <hazem3500@gmail.com>
2022-03-01 15:53:50 +0000

    feat: Add Github repo link

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -4,10 +4,15 @@ import styles from './Header.module.css'
 export function Header() {
   return (
     <header className={styles.header}>
-      <Image src="/logo.svg" width={50} height={50} />
-      <h1>
-        <strong>react three fiber</strong> journey
-      </h1>
+      <div className={styles.titleContainer}>
+        <Image src="/logo.svg" width={50} height={50} alt="" />
+        <h1>
+          <strong>react three fiber</strong> journey
+        </h1>
+      </div>
+      <a className={styles.repoLink} href="https://github.com/pmndrs/threejs-journey" target="_blank" rel="noreferrer"> 
+        <Image src="/github.svg" alt="Github Icon" width={24} height={24} />
+      </a>
     </header>
   )
 }
